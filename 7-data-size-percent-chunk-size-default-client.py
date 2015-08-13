6d88b41e24f785f3a924004ee782da66da2df24d
CAI Qian <caiqian@redhat.com>
2015-08-13 13:34:26 -0400

    Allow an argument for different root VG PV.

--- 7-data-size-percent-chunk-size-default-client.py
+++ 7-data-size-percent-chunk-size-default-client.py
@@ -3,15 +3,19 @@
 # 1. root filesystem is in a VG.
 # 2. have free space in root VG to build up a docker pool.
 # 3. additional extra disk
-import dsstst, os
+import dsstst, os, sys
 print "- start: " + os.path.basename(__file__)
 dsstst.check_root()
 dsstst.destroy_pool_and_extra_disk()
 vg=dsstst.get_rootvg()
-oldpe = dsstst.oldpe + dsstst.get_freepe(dev = dsstst.base)
+if len(sys.argv) > 1:
+	base = sys.argv[1]
+else:
+	base = dsstst.base
+oldpe = dsstst.oldpe + dsstst.get_freepe(dev = base)
 dsstst.conf_and_start_docker(conf='DATA_SIZE=80%FREE\nDEVS=' + dsstst.extra)
 dsstst.check_chunksize(size=512, vg=vg)
 dsstst.check_percent(
-	new = dsstst.get_freepe(dsstst.base) + dsstst.get_freepe(dsstst.pextra),
+	new = dsstst.get_freepe(base) + dsstst.get_freepe(dsstst.pextra),
 		old = oldpe , percent = 0.8)
 print "- pass: " + os.path.basename(__file__)
--- 8-data-size-chunk-size-client.py
+++ 8-data-size-chunk-size-client.py
@@ -3,11 +3,15 @@
 # 1. root filesystem is in a VG.
 # 2. have free space in root VG to build up a docker pool.
 # 3. additional extra disk
-import dsstst, os
+import dsstst, os, sys
 print "- start: " + os.path.basename(__file__)
 dsstst.check_root()
 dsstst.destroy_pool_and_extra_disk()
-oldpe = dsstst.get_freepe(dsstst.base) + dsstst.oldpe
+if len(sys.argv) > 1:
+	base = sys.argv[1]
+else:
+	base = dsstst.base
+oldpe = dsstst.get_freepe(base) + dsstst.oldpe
 # Fill into 70% of free space to trigger auto-extension.
 size = str(int(32 * oldpe * 0.7)) + 'MB'
 vg=dsstst.get_rootvg()
@@ -15,6 +19,6 @@ dsstst.conf_and_start_docker(
 	conf='DATA_SIZE=' + size + '\nCHUNK_SIZE=1024\nDEVS=' + dsstst.extra)
 dsstst.check_chunksize(size=1024, vg=vg)
 dsstst.check_percent(
-	new = dsstst.get_freepe(dsstst.base) +
+	new = dsstst.get_freepe(base) +
 		dsstst.get_freepe(dsstst.pextra), old = oldpe, percent=0.7)
 print "- pass: " + os.path.basename(__file__)
